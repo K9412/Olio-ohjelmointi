cac826bfa9ac9e3f180bda8fc8e0c03d4246769f
Jusa <K9412@student.jamk.fi>
2017-03-12 21:42:07 +0200

    Labra8T5

---
+++ Labra8T5/Labra8T5/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Labra8T5
+{
+    public class ArrayCalcs
+    {
+        public static double Sum(double[] array)
+        {
+            return array.Sum();
+        }
+        public static double Average(double[] array)
+        {
+            return array.Average();
+        }
+        public static double Min(double[] array)
+        {
+            return array.Min();
+        }
+        public static double Max(double[] array)
+        {
+            return array.Max();
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] array = { 1.0, 2.0, 3.3, 5.5, 6.3, -4.5, 12.0 };
+
+            Console.WriteLine("Sum = " + ArrayCalcs.Sum(array));
+            Console.WriteLine("Ave = " + ArrayCalcs.Average(array));
+            Console.WriteLine("Min = " + ArrayCalcs.Min(array));
+            Console.WriteLine("Max = " + ArrayCalcs.Max(array));
+        }
+    }
+}
---
+++ Labra8T5/Labra8T5Tests/ArrayCalcsTests.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Labra8T5;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Labra8T5.Tests
+{
+    [TestClass()]
+    public class ArrayCalcsTests
+    {
+        [TestMethod()]
+        public void SumTest()
+        {
+            double[] array = { 1.0, 2.0, 3.3, 5.5, 6.3, -4.5, 12.0 };
+            double expected = 25.6;
+
+            double actual = array.Sum();
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void AverageTest()
+        {
+            double[] array = { 1.0, 2.0, 3.3, 5.5, 6.3, -4.5, 12.0 };
+            double expected = 3.65714285714286;
+
+            double actual = array.Average();
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void MinTest()
+        {
+            double[] array = { 1.0, 2.0, 3.3, 5.5, 6.3, -4.5, 12.0 };
+            double expected = -4.5;
+
+            double actual = array.Min();
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void MaxTest()
+        {
+            double[] array = { 1.0, 2.0, 3.3, 5.5, 6.3, -4.5, 12.0 };
+            double expected = 12.0;
+
+            double actual = array.Max();
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
