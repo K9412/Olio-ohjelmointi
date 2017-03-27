7a1b99bbf6ed07cc6bd4d6bf69eea21a5ea0aab8
Jusa <K9412@student.jamk.fi>
2017-03-27 18:31:06 +0300

    Labra9

---
+++ Labra9T2/Labra9T2/MainWindow.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Labra9T2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        double eurot, markat;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void txtMarkat_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            bool test = double.TryParse(txtMarkat.Text, out markat);
+            if (!test)
+            {
+                txtMarkat.Text = "Koita uudestaan";
+            }
+        }
+        private void txtEurot_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            bool test = double.TryParse(txtEurot.Text, out eurot);
+            if (!test)
+            {
+                txtEurot.Text = "Koita uudestaan";
+            }
+        }
+        private void btnEuroiksi_Click(object sender, RoutedEventArgs e)
+        {
+            txtEurot.Text = (markat / 6).ToString("0.00");
+        }
+        private void btmMarkoiksi_Click(object sender, RoutedEventArgs e)
+        {
+            txtMarkat.Text = (eurot * 6).ToString("0.00");
+        }
+    }
+}
---
+++ Labra9T3/Labra9T3/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Labra9T3
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            int ikkunaLeveys, ikkunaKorkeus, karmiLeveys;
+
+            bool test1 = int.TryParse(txtLeveys.Text, out ikkunaLeveys);
+            bool test2 = int.TryParse(txtKorkeus.Text, out ikkunaKorkeus);
+            bool test3 = int.TryParse(txtKarmi.Text, out karmiLeveys);
+
+            if (test1 && test2 && test3)
+            {
+                txtPintaAlaIkkuna.Text = (((ikkunaLeveys + (2 * karmiLeveys)) * (ikkunaKorkeus + (2 * karmiLeveys))) / 10).ToString() + "cm^2";
+                txtPintaAlaLasi.Text = ((ikkunaLeveys * ikkunaKorkeus) / 10).ToString() + "cm^2";
+                txtKarmiPiiri.Text = (((ikkunaLeveys * 2) + (ikkunaKorkeus * 2)) / 10).ToString() + "cm";
+            }
+        }
+    }
+}
