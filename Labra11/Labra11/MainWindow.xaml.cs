039f3c461c5989688155c7dd81b620672ec1f312
Jusa <K9412@student.jamk.fi>
2017-04-10 19:18:33 +0300

    Labra11 kesken

---
+++ Labra11/Labra11/MainWindow.xaml.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Labra11
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        MyColor color;
+        Dictionary<string, MyColor> myColors;
+        public MainWindow()
+        {
+            color = new MyColor();
+            myColors = new Dictionary<string, MyColor>();
+            InitializeComponent();
+            tb1.DataContext = color;
+            tb2.DataContext = color;
+            tb3.DataContext = color;
+            myGrid.Background = (SolidColorBrush)new BrushConverter().ConvertFrom(color.Hex);
+        }
+
+        private void btnSave_Click(object sender, RoutedEventArgs e)
+        {
+            if (myColors.ContainsKey(tbSaveAs.Text))
+            {
+                MessageBox.Show("A color with the specified name already exists.", "Error", MessageBoxButton.OK);
+            }
+            else
+            {
+                myColors.Add(tbSaveAs.Text, color);
+                Button newButton = new Button();
+                newButton.Content = tbSaveAs.Text;
+                newButton.Click += new RoutedEventHandler(newButton_Click);
+                newButton.Background = (SolidColorBrush)new BrushConverter().ConvertFrom(color.Hex);
+                myStackPanel.Children.Add(newButton);
+            }
+        }
+
+        private void newButton_Click(object sender, RoutedEventArgs e)
+        {
+            Button temp = (Button)sender;
+
+            foreach (KeyValuePair<string, MyColor> kvp in myColors)
+            {
+                if (kvp.Key == (string)temp.Content)
+                {
+                    MessageBox.Show((string)temp.Content, (string)temp.Content, MessageBoxButton.OK);
+                }
+            }
+            
+        }
+
+        private void sld_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            color.Red = (int)sldRed.Value;
+            color.Green = (int)sldGreen.Value;
+            color.Blue = (int)sldBlue.Value;
+            tbAsHex.Text = color.Hex;
+            myGrid.Background = (SolidColorBrush)new BrushConverter().ConvertFrom(color.Hex);
+        }
+
+        private void tbSaveAs_GotFocus(object sender, RoutedEventArgs e)
+        {
+            TextBox tb = (TextBox)sender;
+            tb.Text = string.Empty;
+            tb.GotFocus -= tbSaveAs_GotFocus;
+        }
+    }
+}
---
+++ Labra11/Labra11/MyColor.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+using System.Drawing;
+
+namespace Labra11
+{
+    class MyColor : INotifyPropertyChanged
+    {
+        private int red, green, blue;
+
+        public int Red
+        {
+            get
+            {
+                return red;
+            }
+            set
+            {
+                if (red != value)
+                {
+                    red = value;
+                    RaisePropertyChanged("Red");
+                }
+            }
+        }
+        public int Green
+        {
+            get
+            {
+                return green;
+            }
+            set
+            {
+                if (green != value)
+                {
+                    green = value;
+                    RaisePropertyChanged("Green");
+                }
+            }
+        }
+        public int Blue
+        {
+            get
+            {
+                return blue;
+            }
+            set
+            {
+                if (blue != value)
+                {
+                    blue = value;
+                    RaisePropertyChanged("Blue");
+                }
+            }
+        }
+        public string Hex
+        {
+            get
+            {
+                string hex = "#";
+                hex += red.ToString("X2");
+                hex += green.ToString("X2");
+                hex += blue.ToString("X2");
+                return hex;
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void RaisePropertyChanged(string property)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(property));
+        }
+    }
+}
